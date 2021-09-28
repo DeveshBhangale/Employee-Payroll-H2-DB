427781642d3ff6dafc58e3011ecb0887ee517a09
MSDOMAIN1\dbhanga <bhangalecr7@gmail.com>
2021-09-28 11:31:45 +0530

    [Devesh]: initial commit

---
+++ EmployeePayrollH2/src/main/java/com/bridgelabz/employeepayroll/EmployeePayrollH2Application.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employeepayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayrollH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePayrollH2Application.class, args);
+	}
+
+}
