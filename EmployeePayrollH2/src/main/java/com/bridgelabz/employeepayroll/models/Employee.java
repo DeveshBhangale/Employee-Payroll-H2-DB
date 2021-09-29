ee62a41da8edbaecca0e37c52b7a5bd946e99b9b
MSDOMAIN1\dbhanga <bhangalecr7@gmail.com>
2021-09-30 00:17:52 +0530

    [Devesh]: implemented employeepayroll lambok in Spring boot

--- EmployeePayrollH2/src/main/java/com/bridgelabz/employeepayroll/models/Employee.java
+++ EmployeePayrollH2/src/main/java/com/bridgelabz/employeepayroll/models/Employee.java
@@ -1,9 +1,11 @@
 package com.bridgelabz.employeepayroll.models;
 
+import lombok.Data;
+
 import javax.persistence.Table;
 
 @Table(name="employee_payroll1")
-public class Employee {
+public @Data class Employee {
 	private int id;
 	private String name;
 	private double salary;
@@ -14,30 +16,6 @@ public class Employee {
 		this.name = employeePayrollDTO.name;
 		this.salary = employeePayrollDTO.salary;
 	
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public double getSalary() {
-		return salary;
-	}
-
-	public void setSalary(double salary) {
-		this.salary = salary;
 	}
 
 	@Override
--- EmployeePayrollH2/src/main/java/com/bridgelabz/employeepayroll/models/EmployeePayrollDTO.java
+++ EmployeePayrollH2/src/main/java/com/bridgelabz/employeepayroll/models/EmployeePayrollDTO.java
@@ -1,6 +1,8 @@
 package com.bridgelabz.employeepayroll.models;
 
-public class EmployeePayrollDTO {
+import lombok.Data;
+
+public @Data class EmployeePayrollDTO {
 	public String name;
 	public long salary;
 	
@@ -9,20 +11,7 @@ public class EmployeePayrollDTO {
 		this.name = name;
 		this.salary = salary;
 	}
-	
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public long getSalary() {
-		return salary;
-	}
-	public void setSalary(long salary) {
-		this.salary = salary;
-	}
-	
+
 	@Override
 	public String toString() {
 		return "EmployeePayrollDTO [name=" + name + ", salary=" + salary + "]";
